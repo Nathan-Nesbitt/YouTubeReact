3f080259cb72294abdbffe8c4fe50288bb530db4
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-06-14 16:50:19 -0700

    Added deployment scripts

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const express = require('express')
-
+const path = require('path');
 const app = express()
 const port = process.env.PORT || 3001;
 
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var youtubeRouter = require('./routes/Youtube').router;
 
 app.use('/youtube/', youtubeRouter);
+
+if (process.env.PRODUCTION) {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '/../../build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../../build', 'index.html'));
+    });
+}
 
 app.listen(port);
 
